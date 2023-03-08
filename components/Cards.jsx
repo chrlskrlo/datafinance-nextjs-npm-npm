f0c966381ee3549cdec46eed728fa7db6d44f678
Charles Karlo Leguarda <charlesleguarda@gmail.com>
2023-03-08 10:43:55 +0800

    Update Cards.jsx
    Changing the text on  cards.jsx

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -13,12 +13,12 @@ const Cards = () => {
                     <Image className='w-20 mx-auto mt-[-3rem]'
                         src={Single}
                         unoptimized={true} />
-                        <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
+                        <h2 className='text-2xl font-bold text-center py-8'>One User</h2>
                         <p className='text-center text-4xl font-bold'>₱8,200</p>
                         <div className='text-center font-medium'>
-                            <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
+                            <p className='py-2 border-b mx-8 mt-8'>100 GB Storage</p>
                             <p className='py-2 border-b mx-8'>1 Granted User</p>
-                            <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
+                            <p className='py-2 border-b mx-8'>Send up to 50 GB</p>
                         </div>
                         <button className='bg-[#da54ff] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
                 </div>
@@ -27,12 +27,12 @@ const Cards = () => {
                     <Image className='w-20 mx-auto mt-[-3rem] bg-white'
                         src={Double}
                         unoptimized={true} />
-                        <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-                        <p className='text-center text-4xl font-bold'>₱8,200</p>
+                        <h2 className='text-2xl font-bold text-center py-8'>Two User</h2>
+                        <p className='text-center text-4xl font-bold'>₱15,500</p>
                         <div className='text-center font-medium'>
                             <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                            <p className='py-2 border-b mx-8'>1 Granted User</p>
-                            <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
+                            <p className='py-2 border-b mx-8'>2 Granted User</p>
+                            <p className='py-2 border-b mx-8'>Send up to 250 GB</p>
                         </div>
                         <button className='bg-black text-[#da54ff] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
                 </div>
@@ -41,12 +41,12 @@ const Cards = () => {
                     <Image className='w-20 mx-auto mt-[-3rem] bg-white'
                         src={Triple}
                         unoptimized={true} />
-                        <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-                        <p className='text-center text-4xl font-bold'>₱8,200</p>
+                        <h2 className='text-2xl font-bold text-center py-8'>Many User</h2>
+                        <p className='text-center text-4xl font-bold'>₱30,200</p>
                         <div className='text-center font-medium'>
-                            <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                            <p className='py-2 border-b mx-8'>1 Granted User</p>
-                            <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
+                            <p className='py-2 border-b mx-8 mt-8'>900 GB Storage</p>
+                            <p className='py-2 border-b mx-8'>5 Granted User</p>
+                            <p className='py-2 border-b mx-8'>Send up to 500 GB</p>
                         </div>
                         <button className='bg-[#da54ff] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
                 </div>
